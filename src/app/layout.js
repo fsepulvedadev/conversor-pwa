63e9bff004ba2b1a91459aa862e7b752f440561b
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-02-17 09:51:31 -0300

    agregado cafecito

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { AppContextProvider } from "@/context/Context";
 import Tarjetas from "../assets/tarjetas.png";
 import Image from "next/image";
+import cafecito from "../assets/cafecito.jpg";
 
 import "./globals.css";
 
@@ -27,12 +28,42 @@ export default function RootLayout({ children }) {
           </div>
           {children}
           <footer className="sticky bottom-0 w-full bg-base-200">
-            <h1 className="w-full text-center text-white ">
-              Desarrollado por{" "}
-              <a className="font-bold" href="https://fsepulveda.vercel.app/">
-                Francisco Sepulveda
-              </a>
-            </h1>
+            <div className="grid grid-cols-2 grid-rows-1 p-1 md:grid-cols-3 place-items-center">
+              <div className="hidden text-base-200 md:block">asd </div>
+
+              <h1 className="w-full text-center text-white ">
+                Desarrollado por <br />
+                <a className="font-bold " href="https://fsepulveda.vercel.app/">
+                  Francisco Sepulveda
+                </a>
+              </h1>
+              <div className="hidden md:block">
+                <p className="mr-2 text-center">Te ayudó la app ?</p>
+                <a
+                  className=""
+                  href="https://cafecito.app/fsepulveda"
+                  rel="noopener"
+                  target="_blank"
+                >
+                  <img
+                    srcset="https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x"
+                    src="https://cdn.cafecito.app/imgs/buttons/button_1.png"
+                    alt="Invitame un café en cafecito.app"
+                  />
+                </a>
+              </div>
+              <div className="flex items-center md:hidden">
+                <p className="mr-2 text-center">Te ayudó la app ?</p>
+                <a
+                  className="cursor-pointer"
+                  href="https://cafecito.app/fsepulveda"
+                  rel="noopener"
+                  target="_blank"
+                >
+                  <Image className="w-10 rounded-2xl" src={cafecito}></Image>
+                </a>
+              </div>
+            </div>
           </footer>
         </body>
       </AppContextProvider>
